f1da97042a04cf3a566551e1a877fdf265604e4f
TacoFork <davidkim654@gmail.com>
2021-08-04 09:32:30 -0400

    added secure page showing logged in user infromation

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
+    @Autowired
+    UserRepository userRepository;
+
+    @RequestMapping("/secure")
+    public String secure(Principal principal, Model model){ //principal has all the information on the user logged in
+        String username = principal.getName();
+        model.addAttribute("user", userRepository.findByUsername(username));
+        return "secure";
+    }
 
     @RequestMapping("/")
-    public String index(){
+    public String index(Principal principal, Model model){
         return "index";
     }
 
--- src/main/java/com/example/demo/UserRepository.java
+++ src/main/java/com/example/demo/UserRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long>{     //Choose the User from the one you created (com.example.demo)
     //CRUD - Create, read, update, delete
+    User findByUsername(String username); //"User" needs to be what is listed in CrudRepository. findByUsername needs to be written exactly the same.
 
 }
